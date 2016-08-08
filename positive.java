1b73ffd9d3574bc6279e0722daa605b036d77c91
Amudha25296 <amutha25296@gmail.com>
2016-08-08 02:03:53 -0700

    Add files via upload

---
+++ positive.java
@@ -0,0 +1,31 @@
+class positive{
+
+public static void main(String[] args) 
+{
+
+Scanner in=new Scanner(System.in);
+
+int a=in.nextInt();
+
+if(a==0)
+
+{
+
+System.out.println("a is neither positive nor negative");
+
+}
+else if(a<0)
+{
+
+System.out.println("a is negative");
+
+}
+
+else
+
+{
+
+System.out.println("a is positive");
+
+}
+}}
